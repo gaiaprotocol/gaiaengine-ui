57afcebca2b93a5e17115f8a7d7a010d658e4bbc
yjgaia <yjdev88@gmail.com>
2025-02-15 13:05:24 +0900

    Add GameStructuredModal class for structured modal dialogs

---
+++ src/dialogs/GameStructuredModal.ts
@@ -0,0 +1,8 @@
+import { DomChild, DomNode } from "@common-module/app";
+import { StructuredModal } from "@common-module/app-components";
+
+interface GameStructuredModalOptions {
+}
+
+export default class GameStructuredModal extends StructuredModal {
+}
